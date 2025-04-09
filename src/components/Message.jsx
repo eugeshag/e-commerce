4dcf51e49826722300bca4f1d611b321d24a23c9
Yevhen <jekayudin228@gmail.com>
2025-04-09 17:17:00 +0200

    fix bug with message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,9 @@
 const Message = ({ messageData }) => {
   if (!messageData) return <></>;
   const { msg, color } = messageData;
-
   return (
     <div
-      className={`fixed top-10 left-1/2 flex items-center justify-center h-7 w-100 -translate-x-1/2 transform bg-${color}-300 rounded-2xl`}
+      className={`fixed top-10 left-1/2 flex h-7 w-100 -translate-x-1/2 transform items-center justify-center ${color === "green" ? "bg-green-300" : ""} ${color === "red" ? "bg-red-300" : ""} rounded-2xl`}
     >
       {msg}
     </div>
