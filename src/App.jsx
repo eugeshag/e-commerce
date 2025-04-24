ce9f03ae40911b2056b25f3327be54e17ced4274
Yevhen <jekayudin228@gmail.com>
2025-04-24 19:13:34 +0200

    fix showing message

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
 import Cart from "./components/Cart";
 import Message from "./components/Message";
+import { useContext } from "react";
+import { CartContext } from "./context/cartContext";
 
 function App() {
+  const {message} = useContext(CartContext)
+  
   return (
     <>
-      {/* <Message messageData={message} /> */}
+      {<Message messageData={message} /> }
       <Routes>
         <Route path="/" element={<Main />} />
         <Route
--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -2,61 +2,71 @@ import { useState, useEffect } from "react";
 import { CartContext } from "./cartContext";
 
 export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
-    const [isCartInitialized, setIsCartInitialized] = useState(false);
-  
-    useEffect(() => {
-      if (isCartInitialized) {
-        localStorage.setItem("cart", JSON.stringify(cart));
+  const [cart, setCart] = useState([]);
+  const [isCartInitialized, setIsCartInitialized] = useState(false);
+  const [message, setMessage] = useState(null)
+
+  useEffect(() => {
+    if (isCartInitialized) {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart, isCartInitialized]);
+
+  useEffect(() => {
+    const savedCart = JSON.parse(localStorage.getItem("cart")) || [];
+    setCart(savedCart);
+    setIsCartInitialized(true);
+  }, []);
+
+  const addToCart = (product) => {
+    setCart((prev) => {
+      const existing = prev.find((item) => item.id === product.id);
+      if (existing) {
+        return prev.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
       }
-    }, [cart, isCartInitialized]);
-  
-    useEffect(() => {
-      const savedCart = JSON.parse(localStorage.getItem("cart")) || [];
-      setCart(savedCart);
-      setIsCartInitialized(true);
-    }, []);
-  
-    const addToCart = (product) => {
-      setCart((prev) => {
-        const existing = prev.find((item) => item.id === product.id);
-        if (existing) {
-          return prev.map((item) =>
-            item.id === product.id
-              ? { ...item, quantity: item.quantity + 1 }
-              : item,
-          );
-        }
-        return [...prev, { ...product, quantity: 1 }];
-      });
-    };
-  
-    const removeFromCart = (productId) => {
-      setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
-    };
-  
-    const decreaseQuantity = (productId) => {
-      setCart((prevCart) => {
-        return prevCart.reduce((acc, item) => {
-          if (item.id === productId) {
-            const newQuantity = item.quantity - 1;
-            if (newQuantity > 0) {
-              acc.push({ ...item, quantity: newQuantity });
-            }
-          } else {
-            acc.push(item);
-          }
-          return acc;
-        }, []);
-      });
-    };
-  
-    return (
-      <CartContext.Provider
-        value={{ cart, addToCart, removeFromCart, decreaseQuantity}}
-      >
-        {children}
-      </CartContext.Provider>
-    );
+      return [...prev, { ...product, quantity: 1 }];
+    });
+    showMessage({color: "green", msg: "Product was added from cart"})
   };
-  
+
+  const removeFromCart = (productId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+    showMessage({color: "red", msg: "Product was removed from cart"})
+  };
+
+  const decreaseQuantity = (productId) => {
+    setCart((prevCart) => {
+      return prevCart.reduce((acc, item) => {
+        if (item.id === productId) {
+          const newQuantity = item.quantity - 1;
+          if (newQuantity > 0) {
+            acc.push({ ...item, quantity: newQuantity });
+          }
+        } else {
+          acc.push(item);
+        }
+        return acc;
+      }, []);
+    });
+    showMessage({color: "red", msg: "Product was removed from cart"})
+  };
+
+  const showMessage = (msg) => {
+    setMessage(msg);
+    setTimeout(() => {
+      setMessage(null);
+    }, 500);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addToCart, removeFromCart, decreaseQuantity, message }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
